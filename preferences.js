ae33d2c7a8b2302a4aaa380328de77d03e9fb29b
kesarirahul <149703616+kesarirahul@users.noreply.github.com>
2025-06-06 14:38:16 +0530

    Delete preferences.js

--- preferences.js
+++
@@ -1,8 +0,0 @@
-function setPreferences(language = "English", theme = "Light", notifications = true) {
-  console.log(`Language: ${language}`);
-  console.log(`Theme: ${theme}`);
-  console.log(`Notifications: ${notifications ? "Enabled" : "Disabled"}`);
-} 
-setPreferences("Hindi", "Dark", false);  
-// setPreferences(undefined, "Dark");
-
