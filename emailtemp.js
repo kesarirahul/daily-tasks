dcbebeb2ae7a09b3e89de14135eb20a88dd12c0d
kesarirahul <149703616+kesarirahul@users.noreply.github.com>
2025-06-06 14:36:10 +0530

    Delete emailtemp.js

--- emailtemp.js
+++
@@ -1,5 +0,0 @@
-function generateemail(Name,dept,DOJ){
-    console.log("Dear " + Name+", Welcome to the "  +dept +  " team! Your joining date is" + DOJ+ ".");
-}
-
-generateemail("Rahul","Internship","14-05-2025");
